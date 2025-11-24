21e64adc661813a719f2cd76181d910f2b8ccf5e
WiryOcean976306 <zhouck777@gmail.com>
2025-11-24 17:53:48 +0000

    Feature B (Fair play martian)

--- ConquestForPlanetX/GameRun.cpp
+++ ConquestForPlanetX/GameRun.cpp
@@ -5,25 +5,27 @@
 #include <time.h>
 #include <unistd.h>
 
-#include "Player.h"
-
 #include <iostream>
+#include <cctype>
 #include <string>
-
-//using namespace std;
 
 #define SIZE 5  //Board size is 5x5 : 3 is the minimum size to fit all characters.
 
 char board[SIZE][SIZE];
 int barLength = (SIZE * 5) + 2;
-bool game_over = false;
+char winner = ' ';
 
 pthread_mutex_t lock;
 
-void print_board() 
+void print_bar()
 {
     std::string repeated(barLength, '-');
     printf("%s\n", repeated.c_str());
+}
+
+void print_board() 
+{
+    print_bar();
 
     for (int i = 0; i < SIZE; i++)
     {
@@ -46,7 +48,7 @@ void board_init()
     }
 }
 
-void character_init(std::string s, char i)
+void character_init(char i)
 {
     int x, y;
     do 
@@ -55,44 +57,112 @@ void character_init(std::string s, char i)
         y = rand() % SIZE;
     } while (board[x][y] != '-');
 
-    // Create a Player object for this character and place it on the board.
-    // Use a simple generated name based on the icon.
-    std::string name = s;
-    ConquestForPlanetX::Player p(name, i, ConquestForPlanetX::Player::Position{x, y});
-
     board[x][y] = i;
 }
 
 void* player_thread(void* arg) 
 {
     char player = *(char*)arg;
-
-    while (!game_over) 
+    while (true) 
     {
         pthread_mutex_lock(&lock);
 
-        int x, y;
+        // If a winner was already found, exit the thread.
+        if (winner != ' ') {
+            pthread_mutex_unlock(&lock);
+            break;
+        }
 
-        // Choose a move
+        int curx = -1, cury = -1;
+        // Find the player's current position on the board
+        for (int i = 0; i < SIZE && curx == -1; ++i) 
+        {
+            for (int j = 0; j < SIZE; ++j) 
+            {
+                if (board[i][j] == player) 
+                {
+                    curx = i;
+                    cury = j;
+                    break;
+                }
+            }
+        }
+
+        if (curx == -1) 
+        {
+            // Player not found on board; nothing to do this turn
+            pthread_mutex_unlock(&lock);
+            sleep(1);
+            continue;
+        }
+
+        bool tryAgain = false;
+        int nx, ny;
+        int attempts = 0;
+        // Choose a neighbouring move (including staying still) but prefer empty cells
         do 
         {
-            x = rand() % SIZE;
-            y = rand() % SIZE;
-        } while (board[x][y] != '-');
+            nx = curx + (rand() % 3) - 1;
+            ny = cury + (rand() % 3) - 1;
+            
+            // safety: if too many attempts, skip turn
+            ++attempts;
+            if (attempts > 50) 
+            {
+                break;
+            }
 
-        // Make the move
-        board[x][y] = player;
+            if (nx < 0 || nx >= SIZE || ny < 0 || ny >= SIZE) 
+            {
+                tryAgain = true;
+            }
+
+        } while (tryAgain || (board[nx][ny] != '-' && board[nx][ny] != 'C' && (board[nx][ny] != 'F' && isupper(player))));
+
+        if (board[nx][ny] == '-') {
+            board[curx][cury] = '-';
+            board[nx][ny] = player;
+        }
+        else if (board[nx][ny] == 'C' && !(isupper(player))) 
+        {
+            player = toupper(player);
+            board[curx][cury] = '-';
+            board[nx][ny] = player;
+        }
+        else if (board[nx][ny] == 'F' && isupper(player)) 
+        {
+            // Declare winner and exit
+            winner = player;
+            board[curx][cury] = '-';
+            board[nx][ny] = player;
+            pthread_mutex_unlock(&lock);
+            return NULL;
+        }
+        
 
         print_board();
-
-        // Check for win or draw
-        // (Win/draw logic to be implemented)
+        print_bar();
+        printf("Player %c moves!\n", player);
 
         pthread_mutex_unlock(&lock);
         sleep(1);  // Wait for a second to avoid the threads cheating by skipping turns.
     }
 
     return NULL;
+}
+
+std::string decypherChar(char w) {
+    switch (w) {
+        case 'b':
+        case 'B': return "Bugs Bunny";
+        case 'd':
+        case 'D': return "Taz";
+        case 't':
+        case 'T': return "Tweety";
+        case 'm':
+        case 'M': return "Marvin the Martian";
+        default: return "Unknown";
+    }
 }
 
 int main() 
@@ -112,19 +182,19 @@ int main()
 
 
     board_init();
-    character_init("Bugs Bunny",Bugs);
-    character_init("Tazmanian Devil", Taz);
-    character_init("Tweety Bird",Tweety);
-    character_init("Marvin the Martion",Marvin);
-    character_init("Mountain", Mountain);
-    character_init("Golden Carrot", Carrot1);
-    character_init("Golden Carrot", Carrot2);
+    character_init(Bugs);
+    character_init(Taz);
+    character_init(Tweety);
+    character_init(Marvin);
+    character_init(Mountain);
+    character_init(Carrot1);
+    character_init(Carrot2);
 
     //Game start
     pthread_create(&bugs, NULL, player_thread, &Bugs);
     pthread_create(&taz, NULL, player_thread, &Taz);
-    pthread_create(&tweety, NULL, player_thread, &tweety);
-    pthread_create(&marvin, NULL, player_thread, &marvin);
+    pthread_create(&tweety, NULL, player_thread, &Tweety);
+    pthread_create(&marvin, NULL, player_thread, &Marvin);
 
     pthread_join(bugs, NULL);
     pthread_join(taz, NULL);
@@ -132,6 +202,15 @@ int main()
     pthread_join(marvin, NULL);
 
     pthread_mutex_destroy(&lock);
+
+
+    print_board();
+    print_bar();
+
+    std::string winnerName = decypherChar(winner);
+    printf("Congratulations to %s for winning the game!\n", winnerName.c_str());
+
+    print_bar();
     
     return 0;
 }
